136bf093d70ce3a63078cfdd66e690a7e09a317d
Bensas <juanbensa@gmail.com>
2022-11-25 18:19:48 -0300

    Cleaned up frontend files

--- frontend/scripts/job-searches.js
+++ frontend/scripts/job-searches.js
@@ -3,7 +3,7 @@ const jobListElement = document.getElementById('job-list');
 getBusquedas().then(function(result){
   console.log('Success, payload', result);
   result.data.items.forEach((jobSearch) => {
-    jobListElement.innerHTML += `<li> <b>${jobSearch.title}:</b> ${jobSearch.description}</li>`
+    jobListElement.innerHTML += `<li class="list-group-item"> <b>${jobSearch.title}:</b> ${jobSearch.description}</li>`
   });
 }).catch( function(result){
   console.log('Error', result);
