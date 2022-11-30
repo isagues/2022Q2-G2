72c920c07670463bb00ef5932a52beed06c8dcdb
Bensas <juanbensa@gmail.com>
2022-11-30 18:39:41 -0300

    Added topbar to identify logged in user (dummy for now), fixed job list parsing

--- frontend/scripts/auth-guard.js
+++ frontend/scripts/auth-guard.js
@@ -8,6 +8,17 @@ fetch('https://' + BASE_URL + '/api/test', {headers: {authorization: idToken}}).
     window.location.href = "login.html";
   }
   else {
+    reflectLoggedInUserInUI('Juan');
     console.log('AuthGuard: authorized');
   }
 }).catch((err) => console.log(err));
+
+
+function reflectLoggedInUserInUI(username) {
+  const loginSignupButtons = document.getElementById('login-signup-buttons');
+  const welcomeMessage = document.getElementById('welcome-message');
+
+  loginSignupButtons.style.display = 'none';
+  welcomeMessage.style.display = 'block';
+  welcomeMessage.innerHTML = `Welcome, ${username}`
+}
--- frontend/scripts/list-job-searches.js
+++ frontend/scripts/list-job-searches.js
@@ -2,8 +2,9 @@ const jobListElement = document.getElementById('job-list');
 
 getBusquedas().then(function(result){
   console.log('Success, payload', result);
-  result.data.items.forEach((jobSearch) => {
-    jobListElement.innerHTML += `<li class="list-group-item"> <b>${jobSearch.title}:</b> ${jobSearch.description}</li>`
+  result.data.Items.forEach((jobSearch) => {
+    jobListElement.innerHTML = '';
+    jobListElement.innerHTML += `<li class="list-group-item"> <b>${jobSearch.title}:</b> ${jobSearch.description}</li>`;
   });
 }).catch( function(result){
   console.log('Error', result);
