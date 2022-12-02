0608fbce35cc7bff67cdbe098e6e71d52078e2f3
Bensas <juanbensa@gmail.com>
2022-12-02 13:20:36 -0300

    Added redirect after creating job search

--- frontend/scripts/create-job-search-form.js
+++ frontend/scripts/create-job-search-form.js
@@ -2,4 +2,10 @@ const createSearchUsername = localStorage.getItem('username');
 const titleField = document.getElementById('job-title-input');
 const descriptionField = document.getElementById('job-description-input');
 const createbutton = document.getElementById('create-job-search-button');
-createbutton.onclick = () => nuevaBusqueda(titleField.value, descriptionField.value, createSearchUsername);
+createbutton.onclick = () => nuevaBusqueda(titleField.value, descriptionField.value, createSearchUsername).then(function(result){
+  console.log('Success, payload', result);
+  window.location.href = 'index.html';
+}).catch( function(result){
+  console.log('Error', result);
+  alert('There was an error while creating the search!');
+});;
